079c51a4a53d716c11360be65a819dd2bb92dfd8
bwilson <bwilson@hortonworks.com>
2016-07-29 15:17:47 -0400

    separated the hostgroups and blueprints specifications when submitting a blueprint

--- submit-blueprint.sh
+++ submit-blueprint.sh
@@ -1,6 +1,10 @@
 # Type must be "single-container" or "multi-container"
 TYPE=$1
+
+#BLUEPRINT must name one of the JSON blueprints in examples/blueprints (e.g., core-hdp)
+BLUEPRINT=$2
+
 source .env
-curl --user admin:admin -H 'X-Requested-By:admin' -X POST $AMBARI_HOST/api/v1/blueprints/multi-container --data-binary "@examples/blueprints/$TYPE.json"
+curl --user admin:admin -H 'X-Requested-By:admin' -X POST $AMBARI_HOST/api/v1/blueprints/${BLUEPRINT} --data-binary "@examples/blueprints/${BLUEPRINT}.json"
 curl --user admin:admin -H 'X-Requested-By:admin' -X PUT $AMBARI_HOST/api/v1/stacks/HDP/versions/$HDP_VERSION/operating_systems/$OS/repositories/HDP-${HDP_VERSION} -d '{"Repositories":{"base_url":"'$BASE_URL'", "verify_base_url":true}}'
 curl --user admin:admin -H 'X-Requested-By:admin' -X POST $AMBARI_HOST/api/v1/clusters/dev --data-binary "@examples/hostgroups/$TYPE.json"
