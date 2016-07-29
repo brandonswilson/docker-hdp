f441e6bb6375ad7912b5526b06573b988e6e04c8
bwilson <bwilson@hortonworks.com>
2016-07-29 17:19:11 -0400

    Added some standard blueprints and fixed a bug in the submit_blueprint_script to map the blueprint name into the hostgroup json

--- submit-blueprint.sh
+++ submit-blueprint.sh
@@ -7,4 +7,8 @@ BLUEPRINT=$2
 source .env
 curl --user admin:admin -H 'X-Requested-By:admin' -X POST $AMBARI_HOST/api/v1/blueprints/${BLUEPRINT} --data-binary "@examples/blueprints/${BLUEPRINT}.json"
 curl --user admin:admin -H 'X-Requested-By:admin' -X PUT $AMBARI_HOST/api/v1/stacks/HDP/versions/$HDP_VERSION/operating_systems/$OS/repositories/HDP-${HDP_VERSION} -d '{"Repositories":{"base_url":"'$BASE_URL'", "verify_base_url":true}}'
-curl --user admin:admin -H 'X-Requested-By:admin' -X POST $AMBARI_HOST/api/v1/clusters/dev --data-binary "@examples/hostgroups/$TYPE.json"
+
+type=$(cat "examples/hostgroups/$TYPE.json")
+type=${type/BLUEPRINT_NAME/$BLUEPRINT}
+curl --user admin:admin -H 'X-Requested-By:admin' -X POST $AMBARI_HOST/api/v1/clusters/dev -d "${type}"
+
